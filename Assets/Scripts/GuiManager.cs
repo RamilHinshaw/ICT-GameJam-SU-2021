dcb0fced236ef2d6eec8dde313438a1055d4518c
RamilHinshaw <RamilHinshaw@gmail.com>
2021-07-31 15:56:41 -0500

    csv exporting

--- Assets/Scripts/GuiManager.cs
+++ Assets/Scripts/GuiManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class GuiManager : MonoBehaviour
 {
@@ -18,7 +19,19 @@ public class GuiManager : MonoBehaviour
 
     public void StartEndScreen()
     {
+        
+    }
 
+    private void WriteToCSV()
+    {
+        string filename = Application.dataPath + "/test.csv";
+        Debug.Log("WRITTEN TO " + filename);
+        TextWriter tw = new StreamWriter(filename);
+
+        tw.WriteLine(Stats.hitObstacles.ToString() + "," + Stats.arrowsUsed.ToString() + "," + Stats.slashUsed.ToString() +
+            "," + Stats.shieldUsed.ToString() + "," + Stats.arrowsHit.ToString() + "," + Stats.slashHit.ToString() + "," + Stats.shieldHit.ToString());
+
+        tw.Close();
     }
 
 }
