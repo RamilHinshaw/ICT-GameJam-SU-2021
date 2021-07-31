683f3c1e4def97936c97ad5b7f2a3f4ac8d0e516
RamilHinshaw <RamilHinshaw@gmail.com>
2021-07-30 22:25:09 -0500

    jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float speed = 1f;
+    public float speed = 1f;
     private int position = 0;
+
+    //jUMP
+    private const float FLOOR_HEIGHT = 4.465f;
+    public float yVelocity = 0;
+    public float jumpPower = 12;
+    public float jumpSpeed = 1f;
+    public float fallSpeed = 9.81f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +29,36 @@ public class PlayerController : MonoBehaviour
         //}
 
         ChangeLanes();
+        Jump();
 
 
     }
 
-    void ChangeLanes()
+    private void Jump()
+    {
+        if (transform.position.y <= FLOOR_HEIGHT && Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("JUMP!");
+            //Jump
+            yVelocity = jumpPower;
+        }
+
+        //IF AIRBORNE FALL
+        if (transform.position.y > FLOOR_HEIGHT)
+        {
+            yVelocity -= fallSpeed * Time.deltaTime;
+        }        
+
+        if (yVelocity <= 0 && (transform.position.y <= FLOOR_HEIGHT))
+        {
+            yVelocity = 0;
+        }
+
+        Vector3 targetYVelocity = new Vector3(transform.position.x, transform.position.y + yVelocity, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetYVelocity, jumpSpeed * Time.deltaTime);
+    }
+
+    private void ChangeLanes()
     {
         if (Input.GetButtonDown("Horizontal"))
         {
