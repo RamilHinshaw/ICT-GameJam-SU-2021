677827bf41b312b53a0788fd49293f31d7c6d164
RamilHinshaw <RamilHinshaw@gmail.com>
2021-08-01 11:49:47 -0500

    intro skip button

---
+++ Assets/Scripts/ControllerLoadLevel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControllerLoadLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetButtonDown("Start"))
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
