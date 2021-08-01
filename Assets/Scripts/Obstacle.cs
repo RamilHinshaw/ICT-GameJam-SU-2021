e11c3679ae28bf959c9802c82315688e8d431b72
RamilHinshaw <RamilHinshaw@gmail.com>
2021-08-01 10:51:20 -0500

    sfx

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -20,5 +20,6 @@ public class Obstacle : MonoBehaviour
     {
         Destroy(gameObject);
         Instantiate(particle, transform.position, transform.rotation);
+        GameManager.Instance.PlayDestroyedSfx();
     }
 }
