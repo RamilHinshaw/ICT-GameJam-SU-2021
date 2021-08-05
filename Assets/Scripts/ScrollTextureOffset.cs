9c23af7e2a8c929ca1fd3a4c549cd416481deea9
RamilHinshaw <RamilHinshaw@gmail.com>
2021-08-05 18:32:12 -0500

    new textures

--- Assets/Scripts/ScrollTextureOffset.cs
+++ Assets/Scripts/ScrollTextureOffset.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class ScrollTextureOffset : MonoBehaviour
 {
     public Renderer rend;
-    public float scrollOffset;
-    public float speed = 1f;
+    //public float scrollOffset;
+    private Vector2 scrollOffset = new Vector2(0, 0);
+    //public float speed = 1f;
+    public Vector2 speed = new Vector2(0, 1);
 
     // Update is called once per frame
     void Update()
     {
-        scrollOffset += Time.deltaTime * speed;
+        //scrollOffset += Time.deltaTime * speed;
+        scrollOffset = new Vector2(scrollOffset.x + (speed.x * Time.deltaTime), scrollOffset.y + (speed.y * Time.deltaTime));
 
-        rend.material.SetTextureOffset("_MainTex", new Vector2(0, scrollOffset));
+        //rend.material.SetTextureOffset("_MainTex", new Vector2(0, scrollOffset));
+        rend.material.SetTextureOffset("_MainTex", scrollOffset);
     }
 }
